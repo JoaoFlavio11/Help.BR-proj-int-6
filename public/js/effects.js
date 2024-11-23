1af7d592855a9f14cb6dabb3e27296cc3ab99b49
JoaoFlavio11 <joaoflaviocl@gmail.com>
2024-11-22 21:18:26 -0300

    atualizações no frontend

--- public/js/effects.js
+++ public/js/effects.js
@@ -5,3 +5,21 @@ const sidebar = document.querySelector("aside");
 menuToggle.addEventListener("click", () => {
   sidebar.classList.toggle("condensed");
 });
+
+//Slider
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function () {
+  nextImage();
+}, 5000)
+
+
+function nextImage() {
+  count++;
+  if (count > 3) {
+    count = 1;
+  }
+  document.getElementById("radio" + count).checked = true;
+}
+//Fim do Slider
